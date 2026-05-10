4c34ed33f75c701f1fb667c7aedb259773a7876f
TY <jacky.tywong@outlook.com>
2026-05-11 00:39:33 +0800

    文案更新

--- nav-bar.js
+++ nav-bar.js
@@ -379,7 +379,7 @@ class NavBar extends HTMLElement {
                         </a>
                         <a href="agent-archives.html" class="dropdown-item">
                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>
-                            小小曜档案库
+                            档案库
                         </a>
                     </div>
                 </div>
